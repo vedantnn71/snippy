1227de47cd76fd09d94195d3a15dd203a475fed4
Vedant Nandwana <vedantnn7.git@gmail.com>
2022-10-03 16:49:54 +0530

    feat(primitives): implement logo component

---
+++ packages/primitives/src/logo.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image"
+import logo from "./assets/logo.svg";
+
+export interface ILogoProps {
+  size?: number;
+}
+
+export const Logo = ({ size = 50 }: ILogoProps) => (
+  <Image src={logo} alt="Snippy" width={size} height={size} />
+)
