82dbe41cf3203751577153f5c3d89c1cbc57f9b2
Vedant Nandwana <vedantnn7.git@gmail.com>
2022-10-03 16:52:07 +0530

    feat(primitives): implement loading spinner component

--- packages/primitives/index.ts
+++ packages/primitives/index.ts
@@ -1,1 +1,7 @@
 export * from "./src/checkbox";
+export * from "./src/logo";
+export * from "./src/icon";
+export * from "./src/loading";
+
+export * as logo from "./src/assets/logo.svg";
+export * as spinner from "./src/assets/spinner.svg";
