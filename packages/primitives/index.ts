63b8228152e28e9e1f58a98e6b2b7b8087f37310
Vedant Nandwana <vedantnn7.git@gmail.com>
2022-10-04 19:36:10 +0530

    feat(primitives): implement alert dialog

--- packages/primitives/index.ts
+++ packages/primitives/index.ts
@@ -4,6 +4,7 @@ export * from "./src/icon";
 export * from "./src/loading";
 export * from "./src/button";
 export * from "./src/toast";
+export * from "./src/alertDialog";
 
 export * as logo from "./src/assets/logo.svg";
 export * as spinner from "./src/assets/spinner.svg";
