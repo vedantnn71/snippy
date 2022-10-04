f0f65e6d3852898d108b7b1aaddb009ce1dacf8b
Vedant Nandwana <vedantnn7.git@gmail.com>
2022-10-04 19:36:33 +0530

    feat(web): implement delete list

--- apps/web/src/components/lists/index.tsx
+++ apps/web/src/components/lists/index.tsx
@@ -1,10 +1,16 @@
 import { useListStore } from "@/store";
 import { trpc } from "@/utils/trpc";
-import { Icon } from "@snippy/primitives";
+import { Icon, AlertDialog } from "@snippy/primitives";
 import { ListsHeader } from "./header";
 
 export const Lists = () => {
   const lists = trpc.list.all.useQuery();
+  const utils = trpc.useContext();
+  const deleteMutation = trpc.list.delete.useMutation({
+    onSuccess: () => {
+      utils.list.all.invalidate();
+    }
+  });
   const setActiveList = useListStore((state) => state.setActiveList);
   const mode = useListStore((state) => state.mode);
 
@@ -16,6 +22,11 @@ export const Lists = () => {
     return !list.isCommandList;
   });
 
+  const deleteList = async (id: string) => {
+    await deleteMutation.mutateAsync(id);
+    return;
+  };
+
   return (
     <div className="align-center border-r-slate-11.5 flex h-screen flex-col border-r-[1px]">
       <ListsHeader />
@@ -24,13 +35,30 @@ export const Lists = () => {
         {listsToShow?.map((list) => (
           <div
             key={list.id}
-            className="flex items-center justify-between p-6 cursor-pointer active:scale-95 transition-all duration-300"
+            className="group flex items-center justify-between p-6 cursor-pointer"
             onClick={() => setActiveList(list.id)}
           >
             <div className="flex items-center gap-2">
               <Icon type="logos" name={list.icon!} size={20} color="#fff" />
               <h1 className="text-md font-medium text-slate-6">{list.name}</h1>
             </div>
+
+            <AlertDialog
+              title={`Delete ${list.name}?`}
+              description="Are you sure you want to delete this list?"
+              confirmLabel="Delete"
+              onConfirm={() => deleteList(list.id)}
+              trigger={
+                <span className="opacity-0 group-hover:opacity-100 group-rdx-state-open:opacity-0 group-active:opacity-0 transition-all duration-500">
+                  <Icon
+                    type="regular"
+                    name="trash"
+                    size={20}
+                    color="var(--red10)"
+                  />
+                </span>
+              }
+            />
           </div>
         ))}
       </div>
