f6341ac5c89fc698e0a0b136bedddd17a74e1b54
Vedant Nandwana <vedantnn7.git@gmail.com>
2022-10-05 21:27:16 +0530

    feat(web/components): add showIconOnly prop in selectIcon

--- apps/web/src/components/selectIcon.tsx
+++ apps/web/src/components/selectIcon.tsx
@@ -6,16 +6,33 @@ import cx from "classnames";
 type ISelectIconProps = {
   value: string;
   setValue: (value: string) => void;
+  showIconOnly?: boolean;
+  onValueChange?: (value: string) => void;
 };
 
-const SelectIcon = ({ value, setValue }: ISelectIconProps) => (
-  <SelectPrimitive.Root defaultValue={value} onValueChange={setValue}>
+export const SelectIcon = ({
+  value,
+  setValue,
+  onValueChange,
+  showIconOnly = false,
+}: ISelectIconProps) => (
+  <SelectPrimitive.Root
+    defaultValue={value}
+    onValueChange={(val) => {
+      setValue(val);
+      onValueChange?.(val);
+    }}
+  >
     <SelectPrimitive.Trigger asChild aria-label="Icons">
       <button className="outline-none flex items-center justify-center mt-1">
-        <SelectPrimitive.Value />
-        <SelectPrimitive.Icon className="ml-2">
-          <ChevronDownIcon />
-        </SelectPrimitive.Icon>
+        {showIconOnly && <Icon type="logos" name={value} size={24} />}
+
+        {!showIconOnly && (
+          <>
+            <SelectPrimitive.Value />
+            <ChevronDownIcon />
+          </>
+        )}
       </button>
     </SelectPrimitive.Trigger>
     <SelectPrimitive.Content>
@@ -24,24 +41,20 @@ const SelectIcon = ({ value, setValue }: ISelectIconProps) => (
       </SelectPrimitive.ScrollUpButton>
       <SelectPrimitive.Viewport className="bg-slate-11.5 p-2 rounded-lg shadow-lg">
         <SelectPrimitive.Group>
-          {icons.map(
-            ({ name, icon }) => (
-              <SelectPrimitive.Item
-                key={name}
-                value={name}
-                className={cx(
-                  "relative flex items-center px-8 py-2 rounded-md text-sm text-gray-300 font-medium gap-2",
-                  "radix-disabled:opacity-50",
-                  "focus:outline-none select-none"
-                )}
-              >
-                {icon}
-                <SelectPrimitive.ItemText>
-                  {name}
-                </SelectPrimitive.ItemText>
-              </SelectPrimitive.Item>
-            )
-          )}
+          {icons.map(({ name, icon }) => (
+            <SelectPrimitive.Item
+              key={name}
+              value={name}
+              className={cx(
+                "relative flex items-center px-8 py-2 rounded-md text-sm text-gray-300 font-medium gap-2",
+                "radix-disabled:opacity-50",
+                "focus:outline-none select-none"
+              )}
+            >
+              {icon}
+              <SelectPrimitive.ItemText>{name}</SelectPrimitive.ItemText>
+            </SelectPrimitive.Item>
+          ))}
         </SelectPrimitive.Group>
       </SelectPrimitive.Viewport>
       <SelectPrimitive.ScrollDownButton className="flex items-center justify-center text-gray-700 dark:text-gray-300">
@@ -51,35 +64,113 @@ const SelectIcon = ({ value, setValue }: ISelectIconProps) => (
   </SelectPrimitive.Root>
 );
 
-export default SelectIcon;
-
 const icons = [
-  { name: "javascript", icon: <Icon type="logos" name="javascript" size={24} color="#fff" /> },
-  { name: "typescript", icon: <Icon type="logos" name="typescript" size={24} color="#fff" /> },
-  { name: "react", icon: <Icon type="logos" name="react" size={24} color="#fff" /> },
-  { name: "vuejs", icon: <Icon type="logos" name="vuejs" size={24} color="#fff" /> },
-  { name: "angular", icon: <Icon type="logos" name="angular" size={24} color="#fff" /> },
-  { name: "html5", icon: <Icon type="logos" name="html5" size={24} color="#fff" /> },
-  { name: "css3", icon: <Icon type="logos" name="css3" size={24} color="#fff" /> },
-  { name: "sass", icon: <Icon type="logos" name="sass" size={24} color="#fff" /> },
-  { name: "tailwind-css", icon: <Icon type="logos" name="tailwind-css" size={24} color="#fff" /> },
-  { name: "bootstrap", icon: <Icon type="logos" name="bootstrap" size={24} color="#fff" /> },
-  { name: "nodejs", icon: <Icon type="logos" name="nodejs" size={24} color="#fff" /> },
-  { name: "git", icon: <Icon type="logos" name="git" size={24} color="#fff" /> },
-  { name: "github", icon: <Icon type="logos" name="github" size={24} color="#fff" /> },
-  { name: "gitlab", icon: <Icon type="logos" name="gitlab" size={24} color="#fff" /> },
-  { name: "docker", icon: <Icon type="logos" name="docker" size={24} color="#fff" /> },
-  { name: "c-plus-plus", icon: <Icon type="logos" name="c-plus-plus" size={24} color="#fff" /> },
-  { name: "python", icon: <Icon type="logos" name="python" size={24} color="#fff" /> },
-  { name: "java", icon: <Icon type="logos" name="java" size={24} color="#fff" /> },
-  { name: "go-lang", icon: <Icon type="logos" name="go-lang" size={24} color="#fff" /> },
-  { name: "flutter", icon: <Icon type="logos" name="flutter" size={24} color="#fff" /> },
-  { name: "mongodb", icon: <Icon type="logos" name="mongodb" size={24} color="#fff" /> },
-  { name: "postgresql", icon: <Icon type="logos" name="postgresql" size={24} color="#fff" /> },
-  { name: "firebase", icon: <Icon type="logos" name="firebase" size={24} color="#fff" /> },
-  { name: "aws", icon: <Icon type="logos" name="aws" size={24} color="#fff" /> },
-  { name: "heroku", icon: <Icon type="logos" name="heroku" size={24} color="#fff" /> },
-  { name: "netlify", icon: <Icon type="logos" name="netlify" size={24} color="#fff" /> },
-  { name: "wordpress", icon: <Icon type="logos" name="wordpress" size={24} color="#fff" /> },
-]
-
+  {
+    name: "javascript",
+    icon: <Icon type="logos" name="javascript" size={24} color="#fff" />,
+  },
+  {
+    name: "typescript",
+    icon: <Icon type="logos" name="typescript" size={24} color="#fff" />,
+  },
+  {
+    name: "react",
+    icon: <Icon type="logos" name="react" size={24} color="#fff" />,
+  },
+  {
+    name: "vuejs",
+    icon: <Icon type="logos" name="vuejs" size={24} color="#fff" />,
+  },
+  {
+    name: "angular",
+    icon: <Icon type="logos" name="angular" size={24} color="#fff" />,
+  },
+  {
+    name: "html5",
+    icon: <Icon type="logos" name="html5" size={24} color="#fff" />,
+  },
+  {
+    name: "css3",
+    icon: <Icon type="logos" name="css3" size={24} color="#fff" />,
+  },
+  {
+    name: "sass",
+    icon: <Icon type="logos" name="sass" size={24} color="#fff" />,
+  },
+  {
+    name: "tailwind-css",
+    icon: <Icon type="logos" name="tailwind-css" size={24} color="#fff" />,
+  },
+  {
+    name: "bootstrap",
+    icon: <Icon type="logos" name="bootstrap" size={24} color="#fff" />,
+  },
+  {
+    name: "nodejs",
+    icon: <Icon type="logos" name="nodejs" size={24} color="#fff" />,
+  },
+  {
+    name: "git",
+    icon: <Icon type="logos" name="git" size={24} color="#fff" />,
+  },
+  {
+    name: "github",
+    icon: <Icon type="logos" name="github" size={24} color="#fff" />,
+  },
+  {
+    name: "gitlab",
+    icon: <Icon type="logos" name="gitlab" size={24} color="#fff" />,
+  },
+  {
+    name: "docker",
+    icon: <Icon type="logos" name="docker" size={24} color="#fff" />,
+  },
+  {
+    name: "c-plus-plus",
+    icon: <Icon type="logos" name="c-plus-plus" size={24} color="#fff" />,
+  },
+  {
+    name: "python",
+    icon: <Icon type="logos" name="python" size={24} color="#fff" />,
+  },
+  {
+    name: "java",
+    icon: <Icon type="logos" name="java" size={24} color="#fff" />,
+  },
+  {
+    name: "go-lang",
+    icon: <Icon type="logos" name="go-lang" size={24} color="#fff" />,
+  },
+  {
+    name: "flutter",
+    icon: <Icon type="logos" name="flutter" size={24} color="#fff" />,
+  },
+  {
+    name: "mongodb",
+    icon: <Icon type="logos" name="mongodb" size={24} color="#fff" />,
+  },
+  {
+    name: "postgresql",
+    icon: <Icon type="logos" name="postgresql" size={24} color="#fff" />,
+  },
+  {
+    name: "firebase",
+    icon: <Icon type="logos" name="firebase" size={24} color="#fff" />,
+  },
+  {
+    name: "aws",
+    icon: <Icon type="logos" name="aws" size={24} color="#fff" />,
+  },
+  {
+    name: "heroku",
+    icon: <Icon type="logos" name="heroku" size={24} color="#fff" />,
+  },
+  {
+    name: "netlify",
+    icon: <Icon type="logos" name="netlify" size={24} color="#fff" />,
+  },
+  {
+    name: "wordpress",
+    icon: <Icon type="logos" name="wordpress" size={24} color="#fff" />,
+  },
+];
