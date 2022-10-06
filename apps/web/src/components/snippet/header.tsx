ab9251dfd7a63bb2b137569b9c83f38ddcf9f250
Vedant Nandwana <vedantnn7.git@gmail.com>
2022-10-06 16:00:37 +0530

    feat(web/snippet): allow deleting snippet

--- apps/web/src/components/snippet/header.tsx
+++ apps/web/src/components/snippet/header.tsx
@@ -1,14 +1,14 @@
-import { Icon } from "@snippy/primitives";
+import { useEffect, useState } from "react";
 import { useSnippetStore } from "@/store";
 import { trpc } from "@/utils/trpc";
-import { useEffect, useState } from "react";
 import { SelectIcon } from "@/components";
+import { Icon, AlertDialog } from "@snippy/primitives";
 
 export const SnippetHeader = () => {
   const id = useSnippetStore((state) => state.activeSnippet);
+  const setActiveSnippet = useSnippetStore((state) => state.setActiveSnippet);
   const isReadOnly = useSnippetStore((state) => state.isReadOnly);
   const snippetQuery = trpc.snippet.byId.useQuery(id!);
-  const deleteMutation = trpc.snippet.delete.useMutation();
   const snippet = snippetQuery.data;
   const utils = trpc.useContext();
   const [icon, setIcon] = useState<string | null>(null);
@@ -19,6 +19,17 @@ export const SnippetHeader = () => {
       utils.snippet.all.invalidate();
     },
   });
+
+  const deleteMutation = trpc.snippet.delete.useMutation({
+    onSuccess: () => {
+      utils.snippet.all.invalidate();
+      setActiveSnippet(null);
+    },
+  });
+
+  const deleteSnippet = () => {
+    deleteMutation.mutateAsync(id!);
+  }
 
   const updateIcon = (val: string) => {
     updateMutation.mutateAsync({
@@ -68,9 +79,18 @@ export const SnippetHeader = () => {
         {!isReadOnly && (
           <div className="flex items-center ml-auto gap-4">
             <Icon type="regular" name={"share"} size={20} />
-            <div className="hover:text-red-9">
-              <Icon type="regular" name={"trash"} size={20} />
-            </div>
+
+            <AlertDialog
+              title={`Delete ${snippet?.name}?`}
+              description="Are you sure you want to delete this snippet?"
+              confirmLabel="Delete"
+              onConfirm={deleteSnippet}
+              trigger={
+                <div className="hover:text-red-9">
+                  <Icon type="regular" name={"trash"} size={20} />
+                </div>
+              }
+            />
           </div>
       )}
       </div>
